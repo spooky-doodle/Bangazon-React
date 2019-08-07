db1919dbd49d92b8166ce66f7857090d6722a7cd
Colin Sandlin <csandlin010@gmail.com>
2019-08-07 09:38:26 -0700

    base model setup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
 import "./index.css";
-import BeatBox from "./BeatBox";
+import Bangazon from "./Bangazon";
 import AWS from 'aws-sdk';
 
 
 ReactDOM.render(
     <Router>
-        <BeatBox />
+        <Bangazon />
     </Router>,
     document.getElementById("root")
 );
