9d80e04f29a51bd1a59c72487bd2bafbddec07f0
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-08 08:17:32 -0500

    Updated routes

--- src/ApplicationViews.jsx
+++ src/ApplicationViews.jsx
@@ -4,12 +4,21 @@ import { Route, Redirect } from "react-router-dom"
 import './applicationViews.css'
 import Storefront from "./components/storefront/Storefront";
 
+import { CustomerList } from "./components/customers/CustomerList";
+import { CustomerDetail } from "./components/customers/CustomerDetail";
+
 class ApplicationViews extends Component {
     render() {
         return (
             <>
                 <Route exact path="/" render={(props) => {
-                    return <Storefront/>;
+                    return <Storefront {...props}/>;
+                }} />
+                <Route exact path="/customers" render={(props) => {
+                    return <CustomerList {...props}/>;
+                }} />
+                <Route exact path="/customers/:custId(\d+)" render={(props) => {
+                    return <CustomerDetail id={parseInt(props.match.params.custId)} {...props}/>;
                 }} />
 
             </>
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,28 +1,61 @@
 import React, { Component } from 'react'
-import { Menu, Dropdown, Container } from 'semantic-ui-react'
+import { Menu, Dropdown, Container, Button, Label } from 'semantic-ui-react'
 import './navbar.css'
+import { LoginModal  } from '../LoginModal';
+import { withRouter } from "react-router"
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+
+    state = {
+        signinHover: false,
+        isModalVisible: false
+    }
+
+    hide = () => {
+        this.setState({isModalVisible: false});
+    }
+
+
+
     render() {
         return (
             <div className='navbar'>
                 <Menu borderless size='massive' inverted fixed='top' color="black" style={{ minHeight: 80 }}>
                     <Container>
-                        <Menu.Item as='a' href='/' className='logo-name'>
+                        <Menu.Item as='a' onClick={() => this.props.history.push("/")} className='logo-name'>
                             Bangazon
                         </Menu.Item>
-                        <Menu.Item position='right'>
-                            <Dropdown item simple text='Select Data'>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item as='a' href='/'>
-                                        Item 1
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Menu.Item>
+                        <Menu.Menu position="right">
+                            <Menu.Item>
+                                <Dropdown item simple text='Select Data'>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item as='a' href='/'>
+                                            Item 1
+                                        </Dropdown.Item>
+                                        <Dropdown.Item as='a' onClick={() => this.props.history.push("/customers")}>
+                                            Customers
+                                        </Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Menu.Item>
+                            <Menu.Item 
+                                icon="sign-in" 
+                                active={this.state.signinHover}
+                                style={{cursor: 'pointer'}}
+                                onMouseEnter={()=> this.setState({signinHover: true})}
+                                onMouseLeave={()=> this.setState({signinHover: false})}
+                                onClick={() => { this.setState({isModalVisible: true}) }}
+
+                            />
+                        </Menu.Menu>
                     </Container>
                 </Menu>
+                <LoginModal isVisible={this.state.isModalVisible} hide={this.hide}/>
+
+
             </div>
         )
     }
 }
+
+export default withRouter(Navbar);
