b3cc937c5d21a4840cd043f46f118f4a9ebe85b8
Nate Fleming <nfleming4@gmail.com>
2019-08-07 19:30:09 -0700

    created modal for ading a new employee

--- src/components/employees/EmployeeCard.jsx
+++ src/components/employees/EmployeeCard.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import {Card, Image} from 'semantic-ui-react'
+import {Card, Image, Icon} from 'semantic-ui-react'
 import dwight from '../../img/Dwight_.jpg'
+import './employee.css'
 
 
 export default class EmployeeCard extends Component {
     render() {
-        console.log(this.props.employee)
         return (
-            <Card>
+            <Card className='card'>
                 <Image src={dwight}/>
                 <Card.Content style={{ backgroundColor: '' }}>
                     <Card.Header>{this.props.employee.firstName} {this.props.employee.lastName}</Card.Header>
@@ -15,10 +15,10 @@ export default class EmployeeCard extends Component {
                         <p>Dept: {this.props.employee.department.name}</p>
                         {(this.props.employee.isSupervisor === true) ?
                         <p>Supervisor</p> : <p>Assistant to the supervisor</p>}
-                        <p>Computers:</p>
+                            <p> {<Icon name='computer'/>} Computers:</p>
                         {(!this.props.employee.computers[0]) ? <p>None Assigned</p> : 
                         this.props.employee.computers.map(computer => 
-                            <p>{computer.manufacturer} {computer.make}</p>)
+                            <p key={computer.id}>{computer.manufacturer} {computer.make}</p>)
                         }
                     </Card.Description>
                 </Card.Content>
